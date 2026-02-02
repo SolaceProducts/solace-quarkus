f860cd25a7c673187b4df588eab333abeac7a1f4
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2026-02-02 11:50:43 +0530

    moved ack handling to worker pool instead of event loop thread

--- quarkus-solace-jcsmp-messaging-connector/runtime/src/main/java/com/solace/quarkus/messaging/incoming/SolaceAckHandler.java
+++ quarkus-solace-jcsmp-messaging-connector/runtime/src/main/java/com/solace/quarkus/messaging/incoming/SolaceAckHandler.java
@@ -3,13 +3,14 @@ package com.solace.quarkus.messaging.incoming;
 import java.util.concurrent.CompletionStage;
 
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 
 class SolaceAckHandler {
 
     public CompletionStage<Void> handle(SolaceInboundMessage<?> msg) {
         return Uni.createFrom().voidItem()
                 .invoke(() -> msg.getMessage().ackMessage())
-                .runSubscriptionOn(msg::runOnMessageContext)
+                .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
                 .subscribeAsCompletionStage();
     }
 }
